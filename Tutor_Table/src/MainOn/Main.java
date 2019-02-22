cff0563404c013eafeefff4700d004b5ed02638d
m.savichev <m.savichev@it24-2.tis.local>
2019-02-22 09:19:36 +0200

    !

---
+++ Tutor_Table/src/MainOn/Main.java
@@ -0,0 +1,16 @@
+package MainOn;
+
+import java.util.ArrayList;
+
+public class Main {
+	private static MainWindow mW;
+	public static ArrayList<Person> persons = new ArrayList<Person>();
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		mW = new MainWindow();
+		
+	} 
+
+}
+ 
