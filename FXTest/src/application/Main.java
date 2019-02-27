dfe617faa067461ca28204bc0806e14c0a6fcbe4
m.savichev <m.savichev@it24-2.tis.local>
2019-02-27 16:30:46 +0200

    Add an fxml file for UI.

--- FXTest/src/application/Main.java
+++ FXTest/src/application/Main.java
@@ -1,9 +1,13 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 
@@ -12,23 +16,27 @@ public class Main extends Application {
 	
 	private Stage stage;
 	private Scene scene;
-	private Label label;
-	private FlowPane pane;
-	
-	@Override
+	private AnchorPane pane;
+	   
+	@Override 
 	public void start(Stage primaryStage) {
 		
 		this.stage = primaryStage;
-		pane = new FlowPane();
+		try {
+			pane = (AnchorPane)FXMLLoader.load(Main.class.getResource("Doctrine.fxml"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		scene = new Scene(pane, 640, 480);
 		stage.setScene(scene);
 		stage.setTitle("Cool things");
-		label = new Label("Hello world on FX now.");
-		pane.getChildren().add(label);
+		
+		
 		stage.show();
-
-//		try { 
-//			BorderPane root = new BorderPane();
+ 
+//		try 
+//			BorderPane root = new BorderPane(); 
 //			Scene scene = new Scene(root,400,400);
 //			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 //			primaryStage.setScene(scene);
